24b1494b0f19cd0e5bb56b608394fbaff4031afd
namik123123 <kristakocourek@gmail.com>
2026-02-19 18:52:59 +0100

    noobie 0.0.1
    only command is say

---
+++ test2.py
@@ -0,0 +1,15 @@
+say = print
+
+
+say("Hello, World!")
+
+elseif = elif
+
+a = 10
+
+if a == 20:
+    say("a is 20")
+elseif a == 10:
+    say("a is 10")
+else:  a == 5
+    say("a is not 10 or 20 its 5")
